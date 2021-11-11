9609047f6b051e431c6ff907fd6b52dbce5597ed
Gulam Faruk <golamfaruk204@gmail.com>
2021-11-11 06:45:33 +0600

    responsive header menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,5 +54,22 @@ $(document).ready(function(){
         $(".search-popup").toggle(500);
       });
 
+      if ($(window).scroll() < 200) {
+          $(".navigation").addClass('sticky');
+          
+      }
+
+          //hamburger Toggle
+    $('.humbarger').click(function(event){
+        $('.menu-list').slideToggle(500);
+        event.preventDefault();
+        
+        $('.menu-list li a').click(function(event) {
+            if ($(window).width() < 768) {
+              $('.menu-list').slideUp(500);
+              event.preventDefault(); 
+            }
+          });
+
   });
  
